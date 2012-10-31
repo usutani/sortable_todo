eb9a57e5fa3e8f9dcad867cafdd98afa1c6d44ed
Yasuhiro Usutani <y_usutani@me.com>
2012-10-31 18:28:36 +0900

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SortableTodo::Application.config.secret_token = 'ac01df6606d448d278e01644d68278bfd750f31cc7c46cc5d80c7892b3032448888f819d3a805e9fb067d2b9af48c33eb47798b5114577a9adcb03ea46b9933f'
